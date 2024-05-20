0a4c67f403dc06fcf42531e94357de497e49e18c
nikita pomidornik <nikitaa95@mail.ru>
2024-05-20 22:03:26 +0300

    hw5 again

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -1,14 +1,12 @@
 let rule = "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
 
 switch (rule.length) {
-    case rule.length < 25:
-        console.log("Все таки нет правил без исключения");
+    case 25:
+        console.log("50 на 50");
         break;
-    case rule.length > 100:
-        console.log("У меня 100 пудов все получится")
-        break
-    case rule.length === 25:
-        console.log("50 на 50")
-        break
-    default: "ыы"
+    case 100:
+        console.log("У меня 100 пудов все получится");
+        break;
+    default:
+        console.log("Все таки нет правил без исключения");
 }
