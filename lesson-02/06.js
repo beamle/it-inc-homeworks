cc099faf1695ae217eefa9827a9e6355670db461
Nikita Alpatov <nikitaa95@mail.ru>
2024-06-09 20:23:01 +0300

    02-02

---
+++ lesson-02/06.js
@@ -0,0 +1,13 @@
+let passportWithAddress = {
+  name: "Petr",
+  surname: "Petrov",
+  address: {
+    country: "USA",
+    city: "LA"
+  }
+};
+
+let passportWithAddressCopy = { ...passportWithAddress, address: { ...passportWithAddress.address, city: "Bobryisk" } };
+
+console.log(passportWithAddress)
+console.log(passportWithAddressCopy)
