251d5e3682e5744d571d9e707272fa1a0b086dc5
Nikita Alpatov <nikitaa95@mail.ru>
2024-06-09 20:24:51 +0300

    04-04

--- lesson-02/04.js
+++ lesson-02/04.js
@@ -1,5 +1,5 @@
 const giveMeRightNumbers = (value1, value2, maximum) => {
-  if (value1 === value2 || value1 <= maximum) {
+  if (value1 === value2 && value1 <= maximum) {
     console.log('Success!');
   } else {
     console.log('Something wrong!');
